Stepped up to float64 as fundamental type. This may fix some numerical instabilities.

math3d/interpolation/r3interpolation.py
@@ -38,7 +38,7 @@ class R3Interpolation(object):
     def pos(self, time, checkrange=True):
         """Called to get the interpolated position at 'time'."""
         if checkrange:
-            time = float(time)
+            time = np.float64(time)
             if time < 0.0 or time > 1.0:
                 raise self.Error('"time" must be number in [0.0 ; 1.0]. Was %f' % time)
         return self._p0 + time * self._displ

math3d/interpolation/se3interpolation.py
@@ -42,7 +42,7 @@ class SE3Interpolation(SO3Interpolation, R3Interpolation):
         """Class callable method for giving the transform at time
         'time'; in [0,1]."""
         if checkrange:
-            time = float(time)
+            time = np.float64(time)
             if time < 0.0 or time > 1.0:
                 raise self.Error('"time" must be number in [0.0 ; 1.0]. Was %f' % time)
         return Transform(self.orient(time, False), self.pos(time, False))

math3d/interpolation/so3interpolation.py
@@ -43,7 +43,7 @@ class SO3Interpolation(object):
     def quat(self, time, checkrange=True):
         """Return the quaternion in the slerp at 'time'; in [0,1]."""
         if checkrange:
-            time = float(time)
+            time = np.float64(time)
             if time < 0.0 or time > 1.0:
                 raise self.Error('"time" must be number in [0.0 ; 1.0]. Was %f' % time) 
         return self._qstart * (self._qstartconjqend) ** time

math3d/orientation.py
@@ -84,8 +84,8 @@ class Orientation(object):
             self._data = np.transpose(np.vstack((va for va in array_args)))
         elif len(args) == 0:
             self._data = np.identity(3)
-        # // Always ensure that we use float32 (single precision floats) as fundamental type
-        self._data=self._data.astype(np.float32)
+        # // Always ensure that we use float64 as fundamental type
+        self._data=self._data.astype(np.float64)
 
     def __copy__(self):
         """Copy method for creating a copy of this Orientation."""

math3d/transform.py
@@ -42,7 +42,7 @@ class Transform(object):
         # if len(arg) == 1 and isSequence(arg[0]):
         #     self.__createOnSequence(arg[0])
         if type(arg) in (tuple, list):
-            self.__create_on_sequence(np.array(arg, dtype=np.float32))
+            self.__create_on_sequence(np.array(arg, dtype=np.float64))
         elif type(arg) == np.ndarray and arg.shape in ((4,4), (3,4)):
             self._o = m3d.Orientation(arg[:3,:3])
             self._v = m3d.Vector(arg[:3,3])
@@ -84,7 +84,7 @@ class Transform(object):
             self._v = m3d.Vector(args[3])
         elif len(args) == 12:
             # // 12 numbers are required
-            args = np.array(args, dtype=float32)
+            args = np.array(args, dtype=float64)
             self._o = m3d.Orientation(args[:9])
             self._v = m3d.Vector(args[9:])
         else:

math3d/utils.py
@@ -13,7 +13,7 @@ __status__ = "Production"
 import numpy as np
 
 ## Possibly replace by 1000*np.finfo(float).resolution
-_eps = np.finfo(np.float32).eps
+_eps = np.finfo(np.float64).eps
 
 ## Tuple of types considered sequences 
 _seqTypes = (list, tuple, np.ndarray)