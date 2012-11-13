Some update to the header info. Properties 'row_matrix' and 'col_matrix' on Vector for getting (1,3) and (3,1) 2d-arrays. Corrected bug in Quaternion constructor on three and four real values. Corrected variable name bug in Quaternion.from_rotation_vector. Properties list and array on all of the classes Quaternion, Vector, Orientation, and Transform. Renamed 'list' property on Transform to 'structured_list' and made 'list' property return the full 4x4 matrix. Introduced a guard for constructing non-unit quaternions.

math3d/quaternion.py
@@ -3,11 +3,11 @@ Module implementing the Quaternion class.
 """
 
 __author__ = "Morten Lind"
-__copyright__ = "Morten Lind 2009-2012"
+__copyright__ = "Morten Lind 2012"
 __credits__ = ["Morten Lind"]
-__license__ = "GPL"
+__license__ = "GPLv3"
 __maintainer__ = "Morten Lind"
-__email__ = "morten@lind.no-ip.org"
+__email__ = "morten@lind.dyndns.dk"
 __status__ = "Production"
 
 import numpy as np
@@ -52,7 +52,7 @@ class Quaternion(object):
             ## Try with rotation vector
             if type(args[0]) == Vector:
                 self.from_rotation_vector(args[0])
-## Copy constructor
+            ## Copy constructor
             elif type(args[0]) == Quaternion:
                 self._s = args[0]._s
                 self._v = args[0]._v.copy()
@@ -67,13 +67,16 @@ class Quaternion(object):
                 axis = args[0].copy()
                 ang = args[1]
                 self.fromAxisAngle(axis, ang)
-        elif len(args) == 3 and np.all(np.isreal):
+        elif len(args) == 3 and np.all(np.isreal(args)):
             ## Assume three components of a rotation vector
             self.from_rotation_vector(Vector(args))
-        elif len(args) == 4 and np.all(np.isreal):
+        elif len(args) == 4 and np.all(np.isreal(args)):
             ## Assume numbers for s, x, y, and z
             self._s = args[0]
             self._v = Vector(args[1:])
+        if np.abs(self.norm() - 1.0) > _eps:
+            print('Quaternion.__init__ : Warning : Arguments did not constitute a unit quaternion. Normalizing.')
+            self.normalize()
 
     def __getattr__(self, name):
         if name == 's':
@@ -229,9 +232,9 @@ class Quaternion(object):
     def from_rotation_vector(self, rot_vec):
         """Set this quaternion to the equivalent of the given
         rotation vector 'w'."""
-        angle = w.length()
+        angle = rot_vec.length()
         if angle > _eps:
-            axis = w.normalized()
+            axis = rot_vec.normalized()
         else:
             ## Select arbitrary x-direction as axis and set angle to zero
             axis = Vector.e1
@@ -240,7 +243,7 @@ class Quaternion(object):
     def fromRotationVector(self, rot_vec):
         _deprecation_warning('fromRotationVector() -> from_rotation_vector()')
         self.from_rotation_vector(rot_vec)
-        
+
     @property
     def rotation_vector(self):
         """Return a rotation vector representing the rotation of this
@@ -370,3 +373,17 @@ class Quaternion(object):
         qi.invert()
         return qi  
 
+    @property
+    def array(self):
+        """ Return an ndarray with the fundamental data
+        of the Quaternion.  The layout is as described by the
+        Quaternion.list property"""
+        return np.array(self.list)
+
+    @property
+    def list(self):
+        """ Return the fundamental data of the Quaternion as a
+        list. The scalar part is placed in the first element, at index
+        0, and the vector data at the remainder, slice [1:]."""
+        return [self._s]+self._v.list
+        

math3d/vector.py
@@ -3,12 +3,13 @@ Module implementing the Vector class.
 """
 
 __author__ = "Morten Lind"
-__copyright__ = "Morten Lind 2009-2012"
+__copyright__ = "Morten Lind 2012"
 __credits__ = ["Morten Lind"]
-__license__ = "GPL"
+__license__ = "GPLv3"
 __maintainer__ = "Morten Lind"
-__email__ = "morten@lind.no-ip.org"
+__email__ = "morten@lind.dyndns.dk"
 __status__ = "Production"
+
 
 import numpy as np
 
@@ -205,9 +206,9 @@ class Vector(object):
 
     @property
     def cross_operator(self):
-        """Return the cross product operator for this
-        Vector. I.e. the skew-symmetric operator Cv, such that Cv * u
-        == v x u, for any vector u."""
+        """Return the cross product operator for this Vector. I.e. the
+        skew-symmetric operator Cv, such that Cv * u == v x u, for any
+        vector u."""
         Cv = np.zeros((3,3))
         Cv[0,1] = -self._data[2]
         Cv[0,2] = self._data[1]
@@ -219,6 +220,29 @@ class Vector(object):
     
     def cross(self, other):
         return Vector(np.cross(self._data, other._data))
+
+    @property
+    def array(self):
+        """ Return a copy of the ndarray which is the fundamental data
+        of the Vector."""
+        return self._data.copy()
+
+    @property
+    def list(self):
+        """ Return the fundamental data of the Vector as a list."""
+        return self._data.tolist()
+        
+    @property
+    def row_matrix(self):
+        """ Property for getting a single-row matrix with the data
+        from the vector."""
+        return self._data.reshape((1,3))
+
+    @property
+    def col_matrix(self):
+        """ Property for getting a single-column matrix with the data
+        from the vector."""
+        return self._data.reshape((3,1))
 
     def __sub__(self, other):
         if type(other) == Vector: