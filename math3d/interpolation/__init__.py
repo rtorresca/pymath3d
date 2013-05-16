ddfd5e7b4aa9065a471c3c527d09088eb5604304
Morten Lind <morten@lind.dyndns.dk>
2013-05-16 08:41:05 +0200

    GPLv3

--- math3d/interpolation/__init__.py
+++ math3d/interpolation/__init__.py
@@ -5,7 +5,7 @@ Top level imports in the interpolation module.
 __author__ = "Morten Lind"
 __copyright__ = "Morten Lind 2009-2012"
 __credits__ = ["Morten Lind"]
-__license__ = "GPL"
+__license__ = "GPLv3"
 __maintainer__ = "Morten Lind"
 __email__ = "morten@lind.no-ip.org"
 __status__ = "Production"
