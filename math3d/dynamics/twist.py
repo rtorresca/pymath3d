Many minor changes to try fixing broken type issues. Mainly originating from the vector iterator. One problem arises that it seems to be ambiguous whether a rotation vector is a Vector or an ndarray.

math3d/dynamics/twist.py
@@ -46,12 +46,12 @@ class OrigoTwist(object):
         elif 'v_lin' in kwargs or 'v_ang' in kwargs:
             self._v_lin = m3d.Vector(kwargs.get('v_lin', m3d.Vector()))
             self._v_ang = m3d.Vector(kwargs.get('v_ang', m3d.Vector()))
+        elif len(args) == 1 and type(args[0]) == OrigoTwist:
+            self._v_lin = args[0].linear
+            self._v_ang = args[0].angular
         elif len(args) == 1 and len(args[0]) == 6:
             self._v_lin = m3d.Vector(args[0][:3])
             self._v_ang = m3d.Vector(args[0][3:])
-        elif len(args) == 1 and type(args[0]) == OrigoTwist:
-            self._v_lin = args[0].v_lin
-            self._v_ang = args[0].v_ang
         elif len(args) == 2 and len(args[0]) == 3 and len(args[1]) == 3:
             self._v_lin = m3d.Vector(args[0])
             self._v_ang = m3d.Vector(args[1])
@@ -90,16 +90,26 @@ class OrigoTwist(object):
             vl_n = ref.orient * self._v_lin + ref.pos.cross(va_n)
             return OrigoTwist(v_lin=vl_n, v_ang=va_n)
     
+    def displacement(self, delta_t):
+        """Compute the displacement resulting from applying the twist
+        for time 'delta_t'. The returned transform will be given in the current
+        coordinates and represent the moved coordinate system."""
+        return m3d.Transform(delta_t * self.array)
+    
     def __rmul__(self, left):
         """Handle a left-operator."""
-        if type(left) in [m3d.Transform, m3d.Vector]:
+        if np.isreal(left):
+            return OrigoTwist(left * self.array)
+        elif type(left) in [m3d.Transform, m3d.Vector]:
             return self.equivalent(left)
-        if type(left) == m3d.Orientation:
+        elif type(left) == m3d.Orientation:
             # Perform a reorientation of the twist, as observed from a
             # coordinate system with the orientation given in 'left'
             vl_n = left * self._v_lin 
             va_n = left * self._v_ang
             return OrigoTwist(v_lin=vl_n, v_ang=va_n)
+        else:
+            return NotImplemented
 
     # Angular property
     def get_angular(self):
@@ -110,6 +120,11 @@ class OrigoTwist(object):
         self._v_ang = m3d.Vector(new_v_ang)
     angular = property(get_angular, set_angular)
 
+    # Raw array data access
+    def get_array(self):
+        return np.append(self._v_lin._data, self._v_ang._data)
+    array = property(get_array)
+
     # Linear property
     def get_linear(self):
         """Get the linear part."""
@@ -118,6 +133,13 @@ class OrigoTwist(object):
         """Set the linear part."""
         self._v_lin = m3d.Vector(new_v_lin)
     linear = property(get_linear, set_linear)
+
+    def __mul__(self, right):
+        """Handle right operation."""
+        if np.isreal(right):
+            return OrigoTwist(right * self.array)
+        else:
+            return NotImplemented
 
     def __add__(self, v_add):
         """Add two twists. Note that they are percieved as belonging

math3d/orientation.py
@@ -256,7 +256,7 @@ class Orientation(object):
         """Return a rotation vector representing this
         orientation. This is essentially the logarithm of the rotation
         matrix. """
-        return self.quaternion.rotation_vector
+        return self.quaternion.rotation_vector._data
     def set_rotation_vector(self, rot_vec):
         """Set this Orientation to represent the one given in a
         rotation vector in 'rot_vec'. 'rot_vec' must be a Vector or an
@@ -400,7 +400,8 @@ class Orientation(object):
         """Return the square of the orientation distance (the angle of
         rotation) to the 'other' orientation.
         """
-        return (self.inverse()*other).rotation_vector.length_sq
+        rv = (self.inverse()*other).rotation_vector
+        return np.dot(rv, rv)
     def angDist2(self, other):
         utils._deprecation_warning('angDist2 -> ang_dist_sq')
         return self.ang_dist_sq(other)

math3d/quaternion.py
@@ -76,8 +76,13 @@ class Quaternion(object):
             ## Assume numbers for s, x, y, and z
             self._s = args[0]
             self._v = Vector(args[1:])
+        else:
+            raise utils.Error(
+                    'Creating on type {} is not supported'
+                    .format(str(type(arg))))
         if np.abs(self.norm - 1.0) > utils._eps:
-            print('Quaternion.__init__ : Warning : Arguments did not constitute a unit quaternion. Normalizing.')
+            print('Quaternion.__init__ : Warning : Arguments did not '
+                  + 'constitute a unit quaternion. Normalizing.')
             self.normalize()
 
     def __getattr__(self, name):
@@ -90,8 +95,8 @@ class Quaternion(object):
         elif name == 'z':
             return self._v.z
         else:
-            raise AttributeError('Attribute "{}" not found in Quaternion '
-                                 + 'class'.format(name))
+            raise AttributeError(('Attribute "{}" not found in Quaternion '
+                                 + 'class').format(name))
 
     @property
     def vector_part(self):
@@ -146,7 +151,7 @@ class Quaternion(object):
         elif type(other) == Quaternion:
             ## Ordinary quaternion multiplication
             return Quaternion(self._s * other._s - self._v * other._v,
-                               self._v.cross(other._v) +
+                              self._v.cross(other._v) +
                               self._s * other._v + other._s * self._v)
         elif utils.is_num_type(other):
             return Quaternion(other * self._s, other * self._v)

math3d/transform.py
@@ -289,7 +289,7 @@ class Transform(object):
     @property
     def pose_vector(self):
         """Get the transform in pose vector representation "(x, y, z, rx, ry, rz)"."""
-        return np.append(self._v._data, self._o.rotation_vector._data)
+        return np.append(self._v._data, self._o.rotation_vector)
 
     @property
     def structured_array(self):

math3d/utils.py
@@ -18,7 +18,8 @@ import numpy as np
 def _deprecation_warning(msg):
     f = inspect.stack()[2]
     # print(f)
-    print('math3d: {} @ {} in {}:\n\tA deprecated method was invoked. '.format(f[1], f[2], f[3])
+    print(('math3d: {} @ {} in {}:\n\tA deprecated method was invoked. ')
+          .format(f[1], f[2], f[3])
           + 'Suggestion for replacement: "%s"' % msg)
     
 ## Limit for accuracy of consistencies and comparison.
@@ -52,6 +53,7 @@ def isNumType(val):
     _deprecation_warning('is_num_type')
     return is_num_type(val)
 def is_num_type(val):
+    # return np.isreal(val)
     return type(val) in _num_types
 
 def isNumTypes(lst):