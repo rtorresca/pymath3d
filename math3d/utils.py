35aa3426916896d1e9e857f4d91d1b4452f347a6
Morten Lind <morten@lind.dyndns.dk>
2014-09-23 14:34:03 +0200

    Some clearing of deprecated code. Better numeric identification in utils module.

--- math3d/utils.py
+++ math3d/utils.py
@@ -27,42 +27,22 @@ _eps = np.finfo(np.float32).resolution
 
 ## Tuple of types considered sequences 
 _seq_types = (list, tuple, np.ndarray)
-_seqTypes = _seq_types
 
-def isSequence(s):
-    _deprecation_warning('is_sequence')
-    return is_sequence(s)
 def is_sequence(s):
     return type(s) in _seq_types
 
-def isThreeSequence(s):
-    _deprecation_warning('is_three_sequence')
-    return is_three_sequence(s)
 def is_three_sequence(s):
     return type(s) in _seq_types and len(s) == 3
 
 ## Standard numeric types
-_num_types = [float, int]
-_numTypes = _num_types
-## Get numeric types from numpy
-for i in np.typeDict:
-    if type(i) == type('') and (i.find('int') >= 0 or i.find('float') >= 0):
-        _num_types.append(np.typeDict[i])
+_number_bases = (np.number,numbers.Number)
         
-def isNumType(val):
-    _deprecation_warning('is_num_type')
-    return is_num_type(val)
 def is_num_type(val):
     # return np.isreal(val)
-    return type(val) in _num_types
+    return isinstance(val, _number_bases)
 
-def isNumTypes(lst):
-    _deprecation_warning('is_num_types')
-    return is_num_types(lst)
 def is_num_types(lst):
-    return np.all([lambda li: isinstance(li, numbers.Number) for li in lst])
-
-
+    return np.all([lambda li: isinstance(li, _number_bases) for li in lst])
     
 class Error(Exception):
     """Exception class."""
