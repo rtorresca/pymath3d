e8a787b50e4c1ffaeac1b589dcc290ef72dd3626
Morten Lind <morten@lind.dyndns.dk>
2015-01-15 11:25:28 +0100

    Updated debian and setup files.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.command.install_data import install_data
 
 setup (
     name = 'math3d', 
-    version = '2.0.0',
+    version = '3.0.1',
     description = '3D Special Euclidean mathematics package for Python.',
     author = 'Morten Lind',
     author_email = 'morten@lind.dyndns.dk',
     url = 'http://launchpad.net/pymath3d',
-    packages = ['math3d', 'math3d.interpolation', 'math3d.reference_system'],
+    packages = ['math3d', 'math3d.interpolation', 'math3d.reference_system', 'math3d.dynamics', 'math3d.geometry'],
     provides = ['math3d'],
     license = 'GNU General Public License v3'
 )
