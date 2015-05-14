87d3f83d5298522723434c6abcbb4d5ce6fd3390
Morten Lind <morten.lind@sintef.no>
2015-05-14 19:21:31 +0200

    Preparing release 3.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.command.install_data import install_data
 
 setup (
     name = 'math3d', 
-    version = '3.0.1',
+    version = '3.0.2',
     description = '3D Special Euclidean mathematics package for Python.',
     author = 'Morten Lind',
     author_email = 'morten@lind.dyndns.dk',
